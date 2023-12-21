f3440d91d3622450aeb423e724a6e7d13f1d1fef
Tunahan Uysal <tunahanuysal26@gmail.com>
2023-12-21 13:34:59 +0300

    fixed the resizing issue. hurray!

--- main.c
+++ main.c
@@ -5,43 +5,37 @@
 #include "stb/stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb/stb_image_write.h"
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#include "stb/stb_image_resize2.h"
 
 int  main(void) {
     int width, height, channels;
-    stbi_set_flip_vertically_on_load(true);  
-    unsigned char *img = stbi_load("proxy-image.png", &width, &height, &channels, 0);
+    // zero seems to be auto assignment? docs dont say shit, not that the docs are good
+    // TODO: create a way to add your file in
+    unsigned char *img = stbi_load("temp.png", &width, &height, &channels, 0);
     if(img != NULL) {
-        unsigned char *new_img = malloc(width * height * channels);
-        if(new_img == NULL) {
-            printf("Error allocating memory for new image\n");
+        printf("%d\n", channels);
+        if(img == NULL) {
+            printf("File could not be loaded.\n");
             stbi_image_free(img);
-            return 1;
+            exit(EXIT_FAILURE);
         }
+        
 
-        for(int i = 0; i < width; i++) {
-            for(int k = 0; k < height; k++) {
-                unsigned char r = img[k*width*channels + i*channels + 0];
-                unsigned char g = img[k*width*channels + i*channels + 1];
-                unsigned char b = img[k*width*channels + i*channels + 2];
-                unsigned char a = img[k*width*channels + i*channels + 3];
-                printf("RGB values of pixel %d %d: %d %d %d %d\n", i, k, r, g, b, a);
-            
-            
-
-                // Fill the new image with the RGB values
-                new_img[k*width*channels + i*channels + 0] = r;
-                new_img[k*width*channels + i*channels + 1] = g;
-                new_img[k*width*channels + i*channels + 2] = b;
-                new_img[k*width*channels + i*channels + 3] = a;
-            }
+        //stbi_write_png("old_image.png", width, height, channels, img, width * channels);
+        unsigned char *output_img = malloc(80 * 80 * channels);
+        // Why does this have two sections where it must be zero? why!!!!!!!
+        stbir_resize_uint8_srgb(img, width, height, 0, output_img, 80, 80, 0, channels);
+        if (output_img != NULL) {
+            printf("Success!\n");
+            stbi_write_png("new_image.png", 80, 80, channels, output_img, 80 * channels);
         }
-        // Write the new image to a PNG file
-        printf(new_img);
-        stbi_write_png("new_image.png", width, height, channels, new_img, width * channels);
-
+        else {
+            printf("sorry\n");
+        }
         // Free the memory used for the new image and the original image
-        free(new_img);
         stbi_image_free(img);
+        free(output_img);
     }
 
     return 0;
