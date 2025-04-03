721ad7f7286ba9d911513d0766eeae5f6e8ae9f4
Tunahan-Uysal <tunahanuysal26@gmail.com>
2025-04-03 16:05:30 +0300

    Removed irrevelant comments in constant.h

--- includes/constants.h
+++ includes/constants.h
@@ -12,26 +12,6 @@ static inline float calculate_charset_divider(const char* charset) {
 }
 
 #define CHARSET_DIVIDER calculate_charset_divider(charset)
-// I have no fucking clue what charset_divider is
-// I just made it up
-// It crashes if its lower than who knows what, i dont even really know what it changes
-// 3.2 crashes
-// 3.5 crashes
-// 3.6 doesnt crash
-// anything above it works
-// 3.55 crashes
-// 3.58 doesnt crash
-// 3.56 crashes
-// 3.57 doesnt crash
-// 3.565 doesnt crash
-// 3.5625 doesnt crash
-// 3.561 doesnt crash
-// 3.5601 doesnt crash
-// 3.56 doesnt crash (???)
-// 3.555 didnt crash
-// 3.5 crashed
-// what even is the pattern
-
 #define DEFAULT_CHARSET "@%&QWNM0gB#$DR8mHXAKUObPd9h6PkqwSE2iF7JvtL?szy*cr!+><;=^,_':.-`";
 
 #define ERROR_NO_PATH 1
